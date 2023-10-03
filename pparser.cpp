2bae5099b8974dbeb73f84407b3889a6cf066870
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-10-03 20:45:41 +0530

    Changes done in lab are added

--- pparser.cpp
+++ pparser.cpp
@@ -12,40 +12,44 @@ class PredParser{
 		string right;
 		string input;
 		bool flag;
+		int input_ptr;
 		bool input_flag;
 		bool ep_flag;
 		set<char> set_nonterminals;
 		set<char> set_terminals;
 		vector<char> nonterminals;
 		vector<char> terminals;
+		vector<char> parse_stack;
 		map<char, bool> first_calculation;
 		map<char, bool> follow_calculation;
 		map<char, vector<string>> grammar;
 		map<char, vector<pair<char,string>>> follow_instances;
 		map<char, vector<string>> first;
 		map<char, vector<string>> follow;
-		map<char, map<string, vector<string>>> table;
+		map<tuple<char, char>, vector<string>> table;
 		bool isNonTerminal(char x);
 		int getIndex(char x);
 		void setIndex();
-		void calcFirst();
-		void calcFollow();
+		void calcFirst(int index);
+		void calcFollow(int index);
+		void simplify();
 		void calcTable();
 		void printFirst();
 		void printFollow();
 		void printTable();
-		void printParserOutput();
+		void printParseOutput(string prod);
 		void setFirstDone(char x);
+		void setFollowDone(char x);
 		bool isFirstDone();
 		bool isFirstCalculated(char x);
 		bool isFollowCalculated(char x);
 		void insert(char left, string right);
 		void findTerminals();
 		void createFollowPrereq();
-		void parse(string x);
+		bool parse();
 		void get();
 	public:
-		void start();
+		void startFunc();
 };
 
 void PredParser:: findTerminals(){
@@ -59,7 +63,7 @@ void PredParser:: findTerminals(){
 			}
 			for(int j=0;j<cur.size();j++){
 				if(!isNonTerminal(cur[j])){
-					set_terminals.push_back(cur[j]);
+					set_terminals.insert(cur[j]);
 				}
 			}
 		}
@@ -68,10 +72,15 @@ void PredParser:: findTerminals(){
 	for(auto x:set_terminals){
 		terminals.push_back(x);
 	}
+	terminals.push_back('$');
 }
 
 void PredParser:: setFirstDone(char x){
 	first_calculation[x] = true;
+}
+
+void PredParser:: setFollowDone(char x){
+	follow_calculation[x] = true;
 }
 
 bool PredParser:: isFirstDone(){
@@ -93,7 +102,7 @@ bool PredParser:: isFirstCalculated(char x){
 	return ans;	
 }
 
-bool PredParser:: isFollowCacluated(char x){
+bool PredParser:: isFollowCalculated(char x){
 	bool ans = follow_calculation[x];
 	return ans;
 }
@@ -110,8 +119,8 @@ int PredParser:: getIndex(char x){
 
 void PredParser:: setIndex(){
 	nonterminals.push_back(start);
-	set<char>::iterator it = unique_nonterminals.begin();
-	while(it!=unique_nonterminals.end()){
+	set<char>::iterator it = set_nonterminals.begin();
+	while(it!=set_nonterminals.end()){
 		if(*it != start){
 			nonterminals.push_back(*it);
 		}
@@ -120,7 +129,7 @@ void PredParser:: setIndex(){
 }
 
 bool PredParser:: isNonTerminal(char x){
-	if(unique_nonterminals.find(x) == unique_nonterminals.end()){
+	if(set_nonterminals.find(x) == set_nonterminals.end()){
 		return false;
 	}
 	return true;
@@ -131,20 +140,20 @@ void PredParser:: insert(char left, string right){
 }
 
 void PredParser:: calcFirst(int index){
-	char current_nt = nonterminals[index];// sort out the logic here!
+	char current_nt = nonterminals[index];
 	vector<string> current_char_prods = grammar[current_nt];
 	string current_prod;
-	char first_char;
 	for(int i=0; i<current_char_prods.size();i++){
 		current_prod = current_char_prods[i];
 		if(current_prod == "epsilon"){
+			first[current_nt].push_back("epsilon");
 			continue;
 		}
 		for(int j=0;j<current_prod.size();j++){
 			ep_flag = false;
-			current_char = current_prod[j];
+			char current_char = current_prod[j];
 			if(isNonTerminal(current_char)){
-				if(first_caculation[current_char]){
+				if(first_calculation[current_char]){
 					for(int k=0;k<first[current_char].size();k++){
 						if(first[current_char][k] == "epsilon"){
 							ep_flag = true;
@@ -162,44 +171,124 @@ void PredParser:: calcFirst(int index){
 				}
 			}
 			else{
-				first[current_nt].push_back(first_char);
+				first[current_nt].push_back(string(1, current_char));
+				break;
 			}
 		}
 	}
 	setFirstDone(current_nt);
-	calcFirst((getIndex(current_nt) + 1);
 }
 
 void PredParser:: calcFollow(int index){
 	char current_nt = nonterminals[index];
-	vector<pair<char,string>> = follow_instances[current_nt];
-	vector<pair<char,string>>::iterator it = follow_instances.begin();
-	while(it != follow_instances.end()){
-		int i = 0;
-		string left_cur = (*it)->first;
-		string prod = (*it)->second;
+	vector<pair<char,string>> current_follow_instances = follow_instances[current_nt];
+	vector<pair<char,string>>::iterator it = current_follow_instances.begin();
+	bool t_flag = false;
+	while(it != current_follow_instances.end()){
+		pair<char, string> p = *it;
+		char left_cur = p.first;
+		string prod = p.second;
 		for(int i=0;i<prod.size();i++){
 			if(prod[i] == current_nt){
+				cout << left_cur << " " << prod << " " << current_nt << "\n";
 				if(i == (prod.size()-1)){
 					if(isFollowCalculated(left_cur)){
 						vector<string> x = follow[left_cur];
-						for(int i=0;i<x.size();i++){
-							follow[current_nt].push_back(x[i]);
+						for(int j=0;j<x.size();j++){
+							follow[current_nt].push_back(x[j]);
 						}
 					}
 					else{
-						calcFollow(getIndex(left_cur));
+						if(left_cur != current_nt){
+							calcFollow(getIndex(left_cur));
+						}
 					}
 				}
 				else{
 					if(isNonTerminal(prod[i+1])){
-						vector<string> x = first[prod[i+1]];
-						for(int i=0;i<x.size();i++){
-							follow[current_nt].push_back(x[i]);
+						int j;
+						for(j=i+1;j<prod.size();j++){
+							vector<string> x = first[prod[j]];
+							t_flag = false;
+							for(int k=0;k<x.size();k++){
+								if(x[k] == "epsilon"){
+									t_flag = true;
+									continue;
+								}
+								follow[current_nt].push_back(x[k]);
+							}
+							if(!t_flag){
+								break;
+							}
+						}
+						if(j == prod.size()){
+							vector<string> x = follow[left_cur];
+							for(int j=0;j<x.size();j++){
+								follow[current_nt].push_back(x[j]);
+							}
+							break;
 						}
 					}
 					else{
-						follow[current_nt].push_back(prod[i+1]);
+						follow[current_nt].push_back(string(1, prod[i+1]));
+					}
+				}
+			}
+		}
+		it++;
+	}
+	setFollowDone(current_nt);
+}
+
+void PredParser:: simplify(){
+	set<string> x;
+	vector<string> y;
+	for(int i=0;i<nonterminals.size();i++){
+		x.clear();
+		y.clear();
+		y = follow[nonterminals[i]];
+		for(int j=0;j<y.size();j++){
+			x.insert(y[j]);
+		}
+		follow[nonterminals[i]].clear();
+		set<string>::iterator it = x.begin();
+		while(it != x.end()){
+			follow[nonterminals[i]].push_back(*it);
+			it++;
+		}
+	}
+}
+
+void PredParser:: calcTable(){
+	findTerminals();
+	map<char, vector<string>>::iterator it = grammar.begin();
+	bool t_flag = false;
+	while(it != grammar.end()){
+		char left = it->first;
+		vector<string> prods = it->second;
+		vector<string>::iterator it1 = prods.begin();
+		t_flag = false;
+		vector<string>::iterator fi = first[left].begin();
+		for(fi;fi != first[left].end();fi++){
+			if(*fi == "epsilon"){
+				t_flag = true;
+				continue;
+			}
+			for(it1; it1 != prods.end(); it1++){
+				string pusher = string(1, left) + " -> " + *it1;
+				tuple<char, char> t = make_tuple(left, (*fi)[0]);
+				table[t].push_back(pusher);
+			}
+		}
+		if(t_flag){
+			vector<string>::iterator fo = follow[left].begin();
+			for(fo;fo != follow[left].end(); fo++){
+				for(it1 = prods.begin(); it1 != prods.end(); it1++){
+					string pusher = string(1, left) + " -> " + *it1;
+					tuple<char, char> t = make_tuple(left, (*fo)[0]);
+					table[t].push_back(pusher);
+					if(table[t].size() > 1){
+						flag = false;
 					}
 				}
 			}
@@ -208,34 +297,59 @@ void PredParser:: calcFollow(int index){
 	}
 }
 
-void PredParse:: calcTable(){
-	map<char, vector<string>>::iterator it = grammar.begin();
-	bool flag = false;
-	while(it != grammar.end()){
-		char left = it->first;
-		flag = false;
-		vector<string>::iterator fi = first[left].begin();
-		for(fi;fi != first[left].end();fi++){
-			if(*fi == "epsilon"){
-				flag = true; 
-			}
-			string pusher = string
-			table[left][*fi].push_back();
+void PredParser:: printFirst(){
+	cout << "NONTERMINAL\t\tFIRST\n";
+	for(int i=0;i<nonterminals.size();i++){
+		cout << nonterminals[i] << "\t\t";
+		vector<string> first_val = first[nonterminals[i]];
+		for(int i=0;i<first_val.size();i++){	
+			cout << first_val[i] << " ";
 		}
-		it++;
+		cout << "\n";
 	}
 }
 
-void printFirst(){
-
+void PredParser:: printFollow(){
+	cout << "NONTERMINAL\t\tFOLLOW\n";
+	for(int i=0;i<nonterminals.size();i++){
+		cout << nonterminals[i] << "\t\t";
+		vector<string> follow_val= follow[nonterminals[i]];
+		for(int i=0;i<follow_val.size();i++){	
+			cout << follow_val[i] << " ";
+		}
+		cout << "\n";
+	}
 }
 
-void printFollow(){
-
+void PredParser:: printTable(){
+	cout << "NONTERMINALS\t";
+	for(int i=0;i<terminals.size();i++){
+		cout << terminals[i] << "\t\t ";
+	}
+	cout << "\n";
+	for(int i=0;i<nonterminals.size();i++){
+		cout << nonterminals[i] << "\t";
+		for(int j=0;j<terminals.size();j++){
+			vector<string> ans = table[make_tuple(nonterminals[i], terminals[j])];
+			for(int k=0;k<ans.size();k++){
+				cout << ans[k] << " ";
+			}
+			cout << "||\t";
+		}
+		cout << "\n";
+	}
 }
 
-void printTable(){
-
+void PredParser:: printParseOutput(string prod){
+	for(int i=0;i<parse_stack.size();i++){
+		cout << parse_stack[i];
+	}
+	cout << "\t\t";
+	for(int i=input_ptr;i<input.size();i++){
+		cout << input[i];
+	}
+	cout << "\t\t";
+	cout << prod << "\n";
 }
 
 void PredParser:: createFollowPrereq(){
@@ -245,15 +359,14 @@ void PredParser:: createFollowPrereq(){
 		it = grammar.begin();
 		while(it != grammar.end()){
 			vector<string>::iterator it1 = it->second.begin();
-			while(it1 != it->second.begin()){
+			while(it1 != it->second.end()){
 				string cur_string = *it1;
 				for(int j=0;j<cur_string.size();j++){
-					if(cur_string[j] == 'cur'){
+					if(cur_string[j] == cur){
 						pair<char, string> p;
 						p.first = it->first;
 						p.second = cur_string;
 						follow_instances[cur].push_back(p);
-						continue;
 					}
 				}
 				it1++;
@@ -263,6 +376,44 @@ void PredParser:: createFollowPrereq(){
 	}
 }
 
+bool PredParser:: parse(){
+	input += "$";
+	input_ptr = 0;
+	parse_stack.push_back('$');
+	parse_stack.push_back(start);
+	cout << "STACK\t\tINPUT\t\tPRODUCTION\n";
+	while(parse_stack[parse_stack.size()-1] != '$'){
+		char top = parse_stack[parse_stack.size()-1];
+		char ip = input[input_ptr];
+		if(top == ip){
+			parse_stack.pop_back();
+			input_ptr++;
+			printParseOutput(" ");
+		}
+		else if(!isNonTerminal(top)){
+			return false;
+		}
+		else if(table[make_tuple(top, ip)].size() == 0){
+			return false;
+		}
+		else{
+			string prod = table[make_tuple(top, ip)][0];
+			printParseOutput(prod);
+			parse_stack.pop_back();
+			for(int i=prod.size()-1;i>-1;i--){
+				if(prod[i] == ' '){
+					break;
+				}
+				parse_stack.push_back(prod[i]);
+			}
+		}
+	}
+	if(input[input_ptr] == '$'){
+		return true;
+	}
+	return false;
+}
+
 void PredParser:: get(){
 	while(true){
 		cout << "Enter the LHS of the production\n";
@@ -270,6 +421,7 @@ void PredParser:: get(){
 		if(left == '#'){
 			break;
 		}
+		set_nonterminals.insert(left);
 		cout << "Enter the RHS of the production\n";
 		cin >> right;
 		insert(left, right);
@@ -279,7 +431,7 @@ void PredParser:: get(){
 	setIndex();
 }
 
-void PredParser:: start(){
+void PredParser:: startFunc(){
 	cout << "Predictive Parser\n";
 	cout << "Enter # while entering the LHS of the production to stop\n";
 	get();
@@ -287,18 +439,20 @@ void PredParser:: start(){
 		first_calculation[nonterminals[i]] = false;
 		follow_calculation[nonterminals[i]] = false;
 	}
-	//probably get this into a method
 	for(int i=0;i<nonterminals.size();i++){
 		if(!isFirstCalculated(nonterminals[i])){
 			calcFirst(i);
 		}
 	}
-	follow[start].push_back('$');
-	for(int i=0<nonterminals.size();i++){
+	follow[start].push_back("$");
+	createFollowPrereq();
+	for(int i=0;i<nonterminals.size();i++){
 		if(!isFollowCalculated(nonterminals[i])){
 			calcFollow(i);
 		}
 	}
+	flag = true;
+	simplify();
 	printFirst();
 	printFollow();
 	calcTable();
@@ -307,9 +461,8 @@ void PredParser:: start(){
 		while(true){
 			cout << "Enter the string to be checked for acceptance in this grammar";
 			cin >> input;
-			input_flag = parse(input);
+			input_flag = parse();
 			if(input_flag){
-				printParseOutput();
 				cout << "The string conforms to the given context-free grammar!\n";
 			}
 			else{
@@ -318,12 +471,12 @@ void PredParser:: start(){
 		}
 	}
 	else{
-		cout << "The given grammar is NOT a LL(1) grammar\n":
+		cout << "The given grammar is NOT a LL(1) grammar\n";
 	}
 }
 
 int main(){
 	PredParser P;
-	P.start();
+	P.startFunc();
 	return 0;
 }
